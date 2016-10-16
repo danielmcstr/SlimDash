a6924afcff519b901cbfecaa7fddaae76bb39b32
Thanh Nguyen <Thanh Nguyen>
2016-10-15 21:29:41 -0500

    update app

---
+++ src/settings.php
@@ -0,0 +1,26 @@
+<?php
+return [
+	'settings' => [
+		'displayErrorDetails' => true,
+		'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+		// Renderer settings
+		'renderer' => [
+			'folders' => [
+				'theme' => APP_PATH . '/themes/app-main',
+			],
+			'ext' => 'twig',
+		],
+		'language' => 'en',
+
+		// Monolog settings
+		'logger' => [
+			'name' => 'slim-app',
+			'path' => APP_PATH . '/logs/app.log',
+		],
+		'session' => [
+			'namespace' => 'slimdash__',
+		],
+		'modules_dir' => APP_PATH . '/modules/',
+		'appmodule' => '\\AppMain\\AppMainModule',
+	],
+];
